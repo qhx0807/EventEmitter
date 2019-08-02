3a5d500526cc53efd2306779622f70ca41b6b744
qhx0807 <820240134@qq.com>
2019-08-02 17:49:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * EventEmiter
+ */
+
+class EventEmiter {
+  constructor () {
+    this.events = Object.create(null)
+  }
+
+  on (type, callback) {
+    this.events[type] = this.events[type] || []
+    if (!Array.isArray(callback)) callback = [callback]
+    for (let i = 0; i < callback.length; i++) {
+      let fn = callback[i]
+      if (typeof fn !== 'function') throw new Error('callback must be a function')
+      this.events[type].push(fn)
+    }
+    return this
+  }
+
+  once (type, fn) {
+    if (typeof fn !== 'function') throw new Error(`'once()' callback must be a function`)
+    function on (...args) {
+      this.off(type, on)
+      fn.call(this, ...args)
+    }
+    on.fn = fn
+    this.on(type, on)
+    return this
+  }
+
+  off (type, fn) {
+    if (this.events[type]) {
+      this.events[type].splice(this.events[type].indexOf(fn), 1)
+      if (this.events[type].length === 0) {
+        delete this.events[type]
+      }
+    }
+    return this
+  }
+
+  emit (type, ...args) {
+    let arr = this.events[type] || []
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].call(this, ...args)
+    }
+    return this
+  }
+
+  removeAll () {
+    this.events = Object.create(null)
+  }
+}
+
+export default EventEmiter
